5dd13de66241191e3522ef336b5315602ac42370
Simmi Walia <simmiw1994@gmail.com>
2023-10-02 14:05:24 +0200

    Added style in user-details and form page

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -32,56 +32,59 @@ export default function Form() {
 
     if (response.status === 201) {
       router.push("/user-details");
+      router.refresh();
     }
   };
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          <span>Name:</span>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
-            type="text"
-            name="name"
-            placeholder="Enter Name"
-            onChange={handleOnChange}
-          />
-        </label>
+    <div className="flex flex-wrap justify-center my-10">
+        <form
+          onSubmit={handleSubmit}
+          className=" bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-sm">
+          <label className="text-gray-700 text-sm font-bold mb-2">
+            <span>Name:</span>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+              type="text"
+              name="name"
+              placeholder="Enter Name"
+              onChange={handleOnChange}
+            />
+          </label>
 
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          <span>Age:</span>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
-            type="number"
-            name="age"
-            placeholder="Enter Age"
-            onChange={handleOnChange}
-          />
-        </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            <span>Age:</span>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+              type="number"
+              name="age"
+              placeholder="Enter Age"
+              onChange={handleOnChange}
+            />
+          </label>
 
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          <span>Country:</span>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
-            type="text"
-            name="country"
-            placeholder="Enter Country"
-            onChange={handleOnChange}
-          />
-        </label>
-        <button
-          name="isLoading"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          disabled={isLoading}>
-          Submit
-        </button>
-      </form>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            <span>Country:</span>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+              type="text"
+              name="country"
+              placeholder="Enter Country"
+              onChange={handleOnChange}
+            />
+          </label>
+          <button
+            name="isLoading"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            disabled={isLoading}>
+            Submit
+          </button>
+        </form>
+        </div>
     </>
   );
 }
--- app/user-details/page.tsx
+++ app/user-details/page.tsx
@@ -3,19 +3,31 @@ import getUserDetails from "../getUserData/fetch";
 export default async function AddUsers() {
   const data = await getUserDetails();
 
+
   return (
     <>
-      <div>
+      <table className=" ml-auto mr-auto my-10">
+        <thead className="bg-gray-300">
+          <tr>
+            <th className=" border px-4 py-2">Name</th>
+            <th className=" border px-4 py-2">Age</th>
+            <th className=" border px-4 py-2">Country</th>
+            
+          </tr>
+        </thead>
+
         {data.map((item: { name: string; age: number; country: string }) => {
           return (
-            <div>
-              <div>{item.name}</div>
-              <div>{item.age}</div>
-              <div>{item.country}</div>
-            </div>
+            <tbody>
+              <tr className="font-mono">
+                <th className="border px-4 py-2">{item.name}</th>
+                <th className="border px-4 py-2">{item.age}</th>
+                <th className="border px-4 py-2">{item.country}</th>
+              </tr>
+            </tbody>
           );
         })}
-      </div>
+      </table>
     </>
   );
 }
